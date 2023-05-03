4765916c2799a81a07f240323e3bbee8c7fd7916
Diana <117006466+damdiana@users.noreply.github.com>
2023-05-03 20:16:06 +0300

    Input icon should be correctly positioned (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Input } from "./components/Input/Input.stories";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [count, setCount] = useState(0);
