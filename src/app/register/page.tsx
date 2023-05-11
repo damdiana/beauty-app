48da0460ef040ecbb6f70fc31a4e6ad1f3f17f52
Diana <117006466+damdiana@users.noreply.github.com>
2023-05-11 13:45:04 +0300

    Create Log in & Sign in Component (#23)

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+import Register from "../../components/auth/Register";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Register />
+    </>
+  );
+}
