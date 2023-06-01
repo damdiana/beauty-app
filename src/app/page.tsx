089c17ef12788d514606fcf0b5494131376d9d97
Diana <117006466+damdiana@users.noreply.github.com>
2023-06-01 19:20:41 +0300

    Insert actual data from the database in homepage (#37)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-"use client";
 import Header from "@/components/Header/Header";
 import ProductCard from "@/components/ProductCard/ProductCard";
+import fetchProducts from "@/services/ProductAPI";
 
-export default function Home() {
+export default async function Home() {
+  let resp = await fetchProducts();
+
   return (
     <>
       <Header
@@ -26,42 +28,15 @@ export default function Home() {
         ]}
       />
       <div className="grid card-grid-cols-3   mt-4 gap-4">
-        <ProductCard
-          product={{
-            brand: "NIOD",
-            title: "Survival 20 Serum 30ml",
-            nrReviews: 9,
-            alt: "NIOD Survival 20 Serum 30ml",
-            src: "https://static.thcdn.com/images/small/webp/productimg/1600/1600/11833538-9874625311016868.jpg",
-            href: "https://www.cultbeauty.com/niod-survival-20-serum-30ml/11833538.html",
-            height: 250,
-            width: 250,
-          }}
-        />
-        <ProductCard
-          product={{
-            brand: "Lancôme",
-            title: "Advanced Génifique Serum 20ml",
-            nrReviews: 9,
-            alt: "serum",
-            src: "https://www.sephora.ro/on/demandware.static/-/Sites-masterCatalog_Sephora/default/dw57c2ffcb/images/hi-res/all/SKU/308937_swatch.jpg",
-            href: "https://www.sephora.ro/p/advanced-genifique-serum---serum-activator-de-tinerete-P10007363.html",
-            height: 250,
-            width: 250,
-          }}
-        />
-        <ProductCard
-          product={{
-            brand: "ALGENIST",
-            title: "GENIUS Liquid Collagen 30ml",
-            nrReviews: 9,
-            alt: "ALGENIST GENIUS Liquid Collagen 30ml",
-            src: "https://static.thcdn.com/images/small/webp//productimg/1600/1600/12676219-6914796886851550.jpg",
-            href: "https://www.cultbeauty.com/algenist-genius-liquid-collagen-supersize-60ml/13222920.html",
-            height: 250,
-            width: 250,
-          }}
-        />
+        {resp.ok === true ? (
+          <>
+            {resp.products.map((product) => {
+              return <ProductCard key={product.id} product={product} />;
+            })}
+          </>
+        ) : (
+          <p className="text-red-500"> {resp.message} </p>
+        )}
       </div>
     </>
   );
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,44 +2,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../Button-Link/Button/Button";
 import Link from "../Button-Link/Link/Link";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { Product } from "@/services/ProductAPI";
 
 type Props = {
-  product: Product;
-};
-
-type Product = {
-  title: string;
-  brand: string;
-  nrReviews: number;
-  src: string;
-  alt: string;
-  href: string;
-  height: number;
-  width: number;
+  product: Omit<Product, "description" | "ingredients">;
 };
 
 const ProductCard = ({ product }: Props) => {
   return (
     <section className="text-center cursor-pointer my-2 py-2">
       <img
-        src={product.src}
-        alt={product.alt}
-        height={product.height}
-        width={product.width}
+        src={product.images[0]}
+        alt={product.name}
+        width="250"
         className="m-auto p-2"
       />
       <div className="flex-col p-2">
-        <Link
-          href={product.href}
-          variant="text"
-          color="black"
-          className="no-underline"
-        >
+        <Link href="#" variant="text" color="black" className="no-underline">
           <span className="font-bold tracking-wide uppercase block mb-0.5">
-            {product.brand}
+            {product.brand_name}
           </span>
-          <p className="font-bold">{product.title} </p>
-          <p> Reviews: {product.nrReviews} </p>
+          <p className="font-bold">{product.name} </p>
         </Link>
         <Button variant="full" color="black" size="medium" className="mt-2">
           Add to favorites <FontAwesomeIcon icon={faHeart} />
