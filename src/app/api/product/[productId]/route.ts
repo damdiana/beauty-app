c93257968c0fafec531fb4d68415d6ae1d78a931
Diana <117006466+damdiana@users.noreply.github.com>
2023-06-07 19:09:03 +0300

    Create GET Product API (#40)

---
+++ src/app/api/product/[productId]/route.ts
@@ -0,0 +1,61 @@
+import { db } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { productId: string };
+  }
+) {
+  try {
+    const client = await db.connect();
+    const { rows: productRows } = await client.sql`SELECT
+        products.id,
+        products.brand_id,
+        brands.name as "brand_name",
+        products.name,
+        products.description,
+        products.ingredients
+    FROM products
+    INNER JOIN brands ON products.brand_id = brands.id
+    WHERE products.id = ${params.productId};
+`;
+
+    if (productRows.length === 0) {
+      return NextResponse.json(
+        {
+          message: "Product not found.",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const { rows: productImagesRows } = await client.sql`SELECT
+	    products.id,
+	    products.name,
+	    productimages.image
+    FROM products
+    INNER JOIN productimages ON products.id = productimages.product_id
+    WHERE products.id = ${params.productId};`;
+
+    productRows[0].images = productImagesRows;
+
+    return NextResponse.json({
+      data: productRows[0],
+    });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      {
+        message: "Interal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
