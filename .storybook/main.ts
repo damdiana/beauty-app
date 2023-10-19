58962fd0430ae1f8bd7ae8d2492d00a1a8f41260
Diana <117006466+damdiana@users.noreply.github.com>
2023-10-19 14:47:46 +0300

    Fix StoryBook (#103)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,15 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/nextjs";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
