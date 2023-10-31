1699afdc002a86db820b9bc70405353e978034ce
Diana <117006466+damdiana@users.noreply.github.com>
2023-10-31 13:57:39 +0200

    Inside the Header component show 3 categories, and then a link called <All Categories> (#102)

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,3 +1,4 @@
+import { HEADER_NAV } from "@/Constants";
 import Header from "@/components/Header/Header";
 import ProfileSideNav from "@/components/ProfileSideNav/ProfileSideNav";
 import getUserServerSide from "@/services/server/UserService";
@@ -15,27 +16,7 @@ export default async function ProfileLayout({
   }
   return (
     <>
-      <Header
-        user={user}
-        nav={[
-          {
-            href: "https://github.com/damdiana?tab=repositories",
-            title: "Body",
-          },
-          {
-            href: "https://github.com/damdiana?tab=repositories",
-            title: "Face",
-          },
-          {
-            href: "https://github.com/damdiana?tab=repositories",
-            title: "New",
-          },
-          {
-            href: "https://github.com/damdiana?tab=repositories",
-            title: "Trending",
-          },
-        ]}
-      />
+      <Header nav={HEADER_NAV} user={user} />
       <main className="h-screen">
         <div className="flex mt-4 w-full h-full">
           <ProfileSideNav />
---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,26 @@
+import { Category } from "@/services/types";
+import Link from "next/link";
+
+const CategoryCard = ({ category }: { category: Category }) => {
+  return (
+    <div className="flex-col p-2 text-center cursor-pointer my-2 py-2">
+      <Link
+        href={`/categories/${category.id}`}
+        color="black"
+        className="no-underline"
+      >
+        <img
+          src="https://picsum.photos/200"
+          alt="https://picsum.photos/200"
+          width="250"
+          className="m-auto p-2"
+        />
+        <p className="font-bold tracking-wide uppercase block mb-0.5">
+          {category.name}
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default CategoryCard;
