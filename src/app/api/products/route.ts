9014847c41512c8e0edb50aab37ecd14c5ca6958
Diana <117006466+damdiana@users.noreply.github.com>
2023-12-18 18:58:13 +0200

    Use the Model function in route, to get all products (#184)

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,50 +1,10 @@
-import { response500 } from "@/app/utils";
-import { getPostgresClient } from "@/services/server/database";
-import { NextResponse } from "next/server";
+import { response200, response500 } from "@/app/utils";
+import { getProducts } from "@/model/ProductModel";
 
 export async function GET(request: Request) {
   try {
-    const client = await getPostgresClient();
-    const [productsResponse, imagesResponse] = await Promise.all([
-      client.query(
-        `SELECT
-        products.id,
-        products.brand_id,
-        brands.name as "brand_name",
-        products.name,
-        products.description,
-        products.ingredients
-    FROM products
-    INNER JOIN brands ON products.brand_id = brands.id;
-  `,
-        []
-      ),
-      client.query(
-        `SELECT
-        products.id,
-        products.name,
-        productimages.image
-      FROM products
-      INNER JOIN productimages ON products.id = productimages.product_id;`
-      ),
-    ]);
-
-    const productRows = productsResponse.rows;
-    const productImagesRows = imagesResponse.rows;
-
-    productRows.forEach((productRow) => {
-      productRow.images = [];
-
-      productImagesRows.forEach((imageRow) => {
-        if (imageRow.id === productRow.id) {
-          productRow.images.push(imageRow.image);
-        }
-      });
-    });
-
-    return NextResponse.json({
-      data: productRows,
-    });
+    let products = await getProducts();
+    return response200({ products });
   } catch (err) {
     console.error(err);
     return response500("Interal server error");
